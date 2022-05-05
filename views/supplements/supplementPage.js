6275cd334849ec1d966ca5cad710cd8d5d70d97f
nikolaspasov <belinelli100@gmail.com>
2022-05-05 16:52:33 +0300

    add error notifications

--- views/supplements/supplementPage.js
+++ views/supplements/supplementPage.js
@@ -14,24 +14,46 @@ function initialize(routerer, renderer) {
 
 function submitHandler(event, supplementKey, supplementName) {
     event.preventDefault();
-    let formData = new FormData(event.target);
-    let unit = formData.get('unit');
-    let value = formData.get('value');
-    console.log(unit);
     
-    if(!window.localStorage.getItem(supplementKey)){
-        let doseObj =  {'name':supplementName, 'key':supplementKey, 'value': value, 'unit': unit};
-        window.localStorage.setItem(supplementKey,JSON.stringify(doseObj));
+
+    try {
+        let formData = new FormData(event.target);
+        let unit = formData.get('unit');
+        let value = formData.get('value');
+        console.log(unit);
+       
+       let errorMessages = [];
+
+       if(supplementKey.trim()===''){
+           errorMessages.push('Supplement is required')
+       }
+       if(value.trim()===''){
+           errorMessages.push('Amount is required')
+       }
+
+       if(errorMessages.length>0){
+           alert(errorMessages.join('\n'));
+           return;
+       }
+       
+        if(!window.localStorage.getItem(supplementKey)){
+            let doseObj =  {'name':supplementName, 'key':supplementKey, 'value': value, 'unit': unit};
+            window.localStorage.setItem(supplementKey,JSON.stringify(doseObj));
+        }
+        else if(JSON.parse(window.localStorage.getItem(supplementKey)).unit === unit){
+            // let previousValue = window.localStorage.getItem(supplementType);
+            // let newValue = Number(previousValue) + Number(value);
+            // window.localStorage.setItem(supplementType, newValue);
+            let supplementEntry = JSON.parse(window.localStorage.getItem(supplementKey));
+            supplementEntry.value = Number(value) + Number(supplementEntry.value);
+            window.localStorage.setItem(supplementKey,JSON.stringify(supplementEntry));
+        }
+        supplementTaken.getView();
+
+    } catch (error) {
+        
     }
-    else if(JSON.parse(window.localStorage.getItem(supplementKey)).unit === unit){
-        // let previousValue = window.localStorage.getItem(supplementType);
-        // let newValue = Number(previousValue) + Number(value);
-        // window.localStorage.setItem(supplementType, newValue);
-        let supplementEntry = JSON.parse(window.localStorage.getItem(supplementKey));
-        supplementEntry.value = Number(value) + Number(supplementEntry.value);
-        window.localStorage.setItem(supplementKey,JSON.stringify(supplementEntry));
-    }
-    supplementTaken.getView();
+    
     
 }
 
